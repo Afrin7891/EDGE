d8ec41ce32bda73219643c3b2b0d9923bc7eb422
Afrin <it23039@mbstu.ac.bd>
2025-05-16 23:03:43 +0600

    Add files via upload

---
+++ Calc.py
@@ -0,0 +1,18 @@
+# Calc.py
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    if a >= b:
+        return a - b
+    else:
+        return b - a
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
